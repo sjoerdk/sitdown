dc431c30d595a834e7330965922513968770d5fc
sjoerd <sjoerd.kerkstra@radboudumc.nl>
2019-05-04 22:08:24 +0200

    Further exploring of gui

--- sitdown/gui/mainwindow.py
+++ sitdown/gui/mainwindow.py
@@ -1,12 +1,16 @@
-from PyQt5.QtWidgets import QApplication, QTextEdit, QMainWindow, QAction
+from PyQt5.QtCore import Qt, QAbstractTableModel, QModelIndex, QVariant
+from PyQt5.QtGui import QStandardItemModel
+from PyQt5.QtWidgets import QApplication, QTextEdit, QMainWindow, QAction, QTreeView
+
+from sitdown.core import Mutation
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
 
-        self.textEdit = QTextEdit()
-        self.setCentralWidget(self.textEdit)
+        self.treeView = QTreeView()
+        self.setCentralWidget(self.treeView)
 
         self.testAction = QAction("Test", self, shortcut="Ctrl+T", triggered=self.test)
 
@@ -15,19 +19,51 @@ class MainWindow(QMainWindow):
         self.statusBar().showMessage("Ready")
 
     def test(self):
-        print('Test!')
+        print("Test!")
 
     def createMenus(self):
         self.fileMenu = self.menuBar().addMenu("&File")
         self.fileMenu.addAction(self.testAction)
 
 
+class MutationsTableModel(QAbstractTableModel):
+    """Shows a list of mutations """
 
-if __name__ == '__main__':
-    import sys
+    column_names = {
+        0: "amount",
+        1: "description",
+        2: "balance_after",
+        3: "date",
+        4: "category",
+        5: "opposite_account",
+    }
 
-    app = QApplication(sys.argv)
-    mainWin = MainWindow()
-    mainWin.show()
-    sys.exit(app.exec_())
+    def __init__(self, mutations, parent=None):
+        """
 
+        Parameters
+        ----------
+        mutations: Set[Mutations]
+        parent: Q
+        """
+        super(MutationsTableModel, self).__init__(parent)
+        self.mutations = list(mutations)
+
+    def rowCount(self, parent=QModelIndex()):
+        return len(self.mutations)
+
+    def columnCount(self, parent=QModelIndex()):
+        return len(self.column_names)
+
+    def data(self, index, role):
+        if not index.isValid() or role != Qt.DisplayRole:
+            return None
+
+        row = index.row()
+        column_name = self.column_names[index.column()]
+        attr_name = column_name
+        return QVariant(str(getattr(self.mutations[row], attr_name)))
+
+    def headerData(self, section, orientation, role):
+        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
+            return self.column_names[section]
--- tests/test_gui.py
+++ tests/test_gui.py
@@ -1,6 +1,8 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QStandardItemModel
 from PyQt5.QtWidgets import QApplication
 
-from sitdown.gui_old.main import Window, createMailModel
+from sitdown.gui.mainwindow import MainWindow, MutationsTableModel
 
 
 def test_gui(short_mutation_sequence):
@@ -13,8 +15,21 @@ def test_gui(short_mutation_sequence):
     import sys
 
     app = QApplication(sys.argv)
-    window = Window()
-    window.setSourceModel(createMailModel(window))
+    window = MainWindow()
+
+    model = QStandardItemModel(0, 2)
+
+    model.setHeaderData(0, Qt.Horizontal, "amount")
+    model.setHeaderData(1, Qt.Horizontal, "descrtiption")
+
+    for mutation in mutations:
+        model.insertRow(0)
+        model.setData(model.index(0, 0), str(mutation.amount))
+        model.setData(model.index(0, 1), mutation.description)
+
+    model = MutationsTableModel(mutations)
+
+    window.treeView.setModel(model)
     window.show()
     sys.exit(app.exec_())
 
