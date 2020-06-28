003dc6574e5ba59fffdaf3131b1144b17d2ef4c7
sjoerdk <sjoerd.kerkstra@radboudumc.nl>
2020-06-28 12:44:32 +0200

    Adds usage and some plot examples

--- docs/usage.rst
+++ docs/usage.rst
@@ -2,6 +2,37 @@
 Usage
 =====
 
-To use sitdown in a project::
+To use sitdown in a project:
 
-    import sitdown
+Download mutations file
+-----------------------
+Currently this library only reader ABN Amro mutation downloads.
+
+* Log in to abnamro.nl > `Zelf regelen` -> `Bij- en afschrijvingen downloaden`
+* Download format TXT. Multiple accounts can be selected, sitdown can separate them later
+
+Load Mutations file
+-------------------
+Suppose for this example that you downloaded the mutations file to `/TXTMutations.TAB` and that
+it contains 100 mutations
+
+In python::
+
+    from sitdown.readers import ABNAMROReader
+    mutations = ABNAMROReader().read('/TXTMutations.TAB')  #  parse all mutations
+
+    >>> mutations  # now this contains 100 Mutation objects
+    Set: 100
+
+    >>> mutation = mutations.pop()  # get the first one from set
+    >>> mutation.date
+    {date} 2019-12-09
+
+    >>> mutation.amount
+    {float} -12.95
+
+    >>> mutations.description
+    {str} 'Tonys lunch room, thank you for you purchase'
+
+    >>> mutations = list(mutations).sort()   # by default, sorts by date, oldest date first
+
--- sitdown/plots.py
+++ sitdown/plots.py
@@ -1,3 +1,51 @@
-""" Different ways of plotting mutations """
+""" Different ways of plotting mutations. Just some examples"""
+from datetime import datetime
+from typing import List
 
-pass
+import matplotlib.pyplot as plt
+
+from sitdown.core import Mutation
+from sitdown.views import MonthSeries, MonthSet
+
+
+def plot_balance(mutations: List[Mutation]):
+    """Plot balance over time. List of mutations should come from a single account"""
+    mutations.sort()
+
+    _, ax = plt.subplots(figsize=(12, 12))
+    ax.set_ylim([0, 1000])
+    ax.grid(b=True, which='both', color='0.65', linestyle='-')
+    ax.set_xlim([datetime.date(year=2019, month=7, day=1),
+                 datetime.date(year=2020, month=2, day=1)])
+    ax.plot([x.date for x in mutations], [x.balance_after for x in mutations])
+    return ax
+
+
+def in_out(mutations: List[Mutation]):
+    """Plot incoming / outgoing total per month for given mutations
+
+    Call plt.show() to actually render this plot
+    """
+    ax = None
+    if not ax:
+        _, ax = plt.subplots(figsize=(12, 12))
+
+    incoming = MonthSeries([x for x in mutations if x.amount > 0])
+    outgoing = MonthSeries([x for x in mutations if x.amount <= 0])
+
+    all = MonthSet(mutations)
+
+    plt.xticks(rotation=70)
+    incoming.plot(ax)
+    outgoing.plot(ax)
+    all.plot(ax)
+
+    diff_text = [f"{x.date} - {x.sum():.1f}" for x in all.bins()]
+    print("\n".join(diff_text))
+
+    diff_text = [f"{x.date} - {x.sum():.1f}" for x in all.bins()]
+
+    ax.grid(b=True, which="both", color="0.65", linestyle="-")
+    ax.set_ylim([-6000, 6000])
+
+    return ax
